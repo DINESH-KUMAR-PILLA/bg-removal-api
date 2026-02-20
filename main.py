4c8d6adb1af1e9aa189dce7c0bb2e82fd99f58f0
PILLA DINESH KUMAR <pilladineshkumar@PILLAs-MacBook-Air.local>
2026-02-20 15:18:42 +0530

    prepare for railway deployment

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
 from rembg import remove, new_session
 import os
 
 app = FastAPI()
+
+# ✅ ADD THIS CORS BLOCK
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # lightweight model
 session = new_session(model_name="u2netp")
